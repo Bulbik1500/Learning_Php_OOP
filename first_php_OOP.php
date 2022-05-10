25fcff7dc5a9570fc2a4823b7ae9700b9a39359d
Bulbik <bulbik1500@gmail.com>
2022-05-10 16:21:29 +0200

    ant much but it honest work

---
+++ first_php_OOP.php
@@ -0,0 +1,14 @@
+<?php
+class Customer{
+    public $id;
+    public $name;
+    public $mail;
+
+    public function getCustomer($id){
+        $this->id = $id;
+        return 'jon';
+    }
+}
+
+$customer  = new Customer;
+echo $customer->getCustomer(1);
